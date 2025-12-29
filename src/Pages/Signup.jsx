92adb6111edde470558b657a0765c4ebba1612f0
chidinma <chidinmaojimba888@gmail.com>
2025-12-29 14:57:45 -0800

    Initial commit

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,45 @@
+import blue from "../assets/blueframe1.png";
+
+const Signup = () => {
+    return ( 
+        <div>
+          <div className="container">
+                <div className="inner-container">
+                        <div className="left-container">
+                            <img src={blue} alt="My blueframe1" className="my-blueframe"/>
+                        </div>
+                        <div className="right-container">
+                            {/* <h1>Create an account</h1> */}
+                         <form>
+                          <h1>Create an account</h1>
+                           <div className="row">
+                            <div className="row-write">
+                            <h4>First Name</h4>               
+                            <input type="text" placeholder="First Name"/>
+                             </div>
+                             <div className="row-write">
+                            <h4>Last Name</h4>
+                            <input type="text" placeholder="Last Name"/>
+                            </div>
+                            </div>
+                             <h4>Email Address</h4>
+                            <input type="email" placeholder="Email address"/>
+                            <h4>Mobile number</h4>
+                            <input type="text" placeholder="Mobile No"/>
+                            <h4>Password</h4>
+                            <input type="password" placeholder="Pluss124"/>
+                            <p>Use 8 or more characters with a mix of numbers, letters & symbols    </p>
+
+                            <button>Create Account</button>
+                            <p className="login">Already have an account?<a href="">Sign in</a></p>
+
+                         </form>
+
+                        </div>
+                </div>
+          </div>
+        </div>
+     );
+}
+ 
+export default Signup;
