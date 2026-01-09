fccd799155f98361a3b1fd052c934f9718662f0d
Obi Uche <ucheobikingsley@gmail.com>
2026-01-10 00:48:33 +0100

    update

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,57 +1,58 @@
 import blue from "../assets/blueframe1.png";
 import { useNavigate } from "react-router-dom";
 
-
 const Signup = () => {
-    const navigate = useNavigate();
-    return ( 
-        <div>
-          <div className="container">
-                <div className="inner-container">
-                        <div className="left-container">
-                            <img src={blue} alt="My blueframe1" className="my-blueframe"/>
-                        </div>
-                        <div className="right-container">
-                            {/* <h1>Create an account</h1> */}
-                        
-                       
-                            
-                         <form>             
-                          <h1>Create an account</h1>
-                           <div className="row">
-                            <div className="row-write">
-                            <h4>First Name</h4>               
-                            <input type="text" placeholder="First Name"/>
-                             </div>
-                             <div className="row-write">
-                            <h4>Last Name</h4>
-                            <input type="text" placeholder="Last Name"/>
-                            </div>
-                            </div>
-                             <h4>Email Address</h4>
-                            <input type="email" placeholder="Email address"/>
-                            <h4>Mobile number</h4>
-                            <input type="text" placeholder="Mobile No"/>
-                            <h4>Password</h4>
-                            <input type="password" placeholder="Pluss124"/>
-                            <p>Use 8 or more characters with a mix of numbers, letters & symbols    </p>
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="container">
+        <div className="inner-container">
+          <div className="left-container">
+            <img src={blue} alt="My blueframe1" className="my-blueframe" />
+          </div>
+          <div className="right-container">
+            {/* <h1>Create an account</h1> */}
 
-                            {/* <button>Create Account</button> */}
-
-                            <button  className="backBttn" onClick={() => navigate("/phoneNumberAut")}>
-                            Create Account
-                            </button>
-                            <p className="login">Already have an account?<a href="">Sign in</a></p>
-
-                         </form>
-
-                        
-
-                        </div>
+            <form>
+              <h1>Create an account</h1>
+              <div className="row">
+                <div className="row-write">
+                  <h4>First Name</h4>
+                  <input type="text" placeholder="First Name" />
                 </div>
+                <div className="row-write">
+                  <h4>Last Name</h4>
+                  <input type="text" placeholder="Last Name" />
+                </div>
+              </div>
+              <h4>Email Address</h4>
+              <input type="email" placeholder="Email address" />
+              <h4>Mobile number</h4>
+              <input type="text" placeholder="Mobile No" />
+              <h4>Password</h4>
+              <input type="password" placeholder="Pluss124" />
+              <p>
+                Use 8 or more characters with a mix of numbers, letters &
+                symbols{" "}
+              </p>
+
+              {/* <button>Create Account</button> */}
+
+              <button
+                className="backBttn"
+                onClick={() => navigate("/phoneNumberAut")}
+              >
+                Create Account
+              </button>
+              <p className="login">
+                Already have an account?<a href="">Sign in</a>
+              </p>
+            </form>
           </div>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Signup;
